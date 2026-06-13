a9c1ea7edfd7ffb2279da5d4070ea1337139e620
Broski <109301662+BitSiphon@users.noreply.github.com>
2026-06-13 19:13:26 -0400

    init templ

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module inventory-thingy
 
 go 1.26.3
+
+require github.com/a-h/templ v0.3.1020 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/a-h/templ"
+)
+
+func main() {
+	index := hello("test")
+
+	http.Handle("/", templ.Handler(index))
+
+	fmt.Println("Listening on :3000")
+	http.ListenAndServe(":3000", nil)
 }
